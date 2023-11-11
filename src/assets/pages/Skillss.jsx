aa6750d2b597154b7a58f8a8fcf106dd1a365f5c
mariaiftikhar1999 <iftikharmaria145@gmail.com>
2023-11-11 17:31:47 +0500

    first commit

---
+++ src/assets/pages/Skillss.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Nav from "../Components/Nav";
+import {data,data2} from "../../data.jsx"
+
+function Skillss() {
+  return (
+    <>
+    <Nav />
+      <div className=" w-full mt-4 rounded-md">
+        <div className="p-6 ">
+          <h3 className="font-medium text-xl">Professional Skills</h3>
+          <div className="flex w-full justify-between">
+            <div className="w-full">
+           
+
+             {
+              data.map((d)=>{
+                return(
+                  <>
+                    <div>
+                <h2 className="font- light pt-4">{d.skills}</h2>
+                <div className=" bg-neutral-200 dark:bg-neutral-600 cardd cd-1 w-1/1">
+                  <div className="bg-blue-500  text-center text-xs font-medium leading-none text-primary-100 w-3/4">
+                    {d.percent}
+                  </div>
+                </div>
+              </div>
+                  </>
+                )
+                 
+
+                
+              })
+
+             }
+            
+            </div>
+            <div className="w-full ml-5">
+            {
+              data2.map((d)=>{
+                return(
+                  <>
+                    <div>
+                <h2 className="font- light pt-4">{d.skills}</h2>
+                <div className=" bg-neutral-200 dark:bg-neutral-600 cardd cd-1 w-1/1">
+                  <div className="bg-blue-500  text-center text-xs font-medium leading-none text-primary-100 w-3/4">
+                    {d.percent}
+                  </div>
+                </div>
+              </div>
+                  </>
+                )
+                 
+
+                
+              })
+
+             }
+             
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Skillss;
---
+++ src/data.jsx
@@ -0,0 +1,37 @@
+export const  data =[
+    {
+    skills:"HTML",
+    percent: "100%"
+
+},
+{
+    skills:"JS",
+    percent: "20%"
+
+},
+{
+    skills:"CSS",
+    percent: "90%"
+
+},
+
+
+
+]
+export const data2 =[
+    {
+        skills:"Brain",
+        percent: "100%"
+    
+    },
+    {
+        skills:"Node",
+        percent: "50%"
+    
+    },
+    {
+        skills:"React",
+        percent: "80%"
+    
+    },
+]
